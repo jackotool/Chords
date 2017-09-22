6a2f168ad181576918a17436982590fe52c7da97
jackotoole <jackotool@gmail.com>
2017-09-22 16:16:29 +1000

    first commit, 22/9/17

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* Jack O'Toole Sep 2017
+	Program to generate guitar chords,ie notes for 6 strings and
+	the fret. 
+	The second objective is to then generate different versions of the chord,
+	 starting with different route note positions and finally inversions.
+	 */
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
